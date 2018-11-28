825c566dea80e721d5db78c0e98af807b8018888
grant smith <grantsmith1256@gmail.com>
2018-11-28 21:45:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
+require_relative '../lib/concerns/memorable.rb'
+
 require 'pry'
 
 class Song
+  extend Memorable
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -16,14 +20,6 @@ class Song
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
